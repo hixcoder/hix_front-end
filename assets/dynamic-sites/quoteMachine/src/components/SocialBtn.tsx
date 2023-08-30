5b74bbece4286ab7c59309c81598652fd6124cd1
hixcoder <hixcoder@gmail.com>
2023-08-30 10:46:40 +0100

    make some updates on quoteMachine

--- assets/dynamic-sites/quoteMachine/src/components/SocialBtn.tsx
+++ assets/dynamic-sites/quoteMachine/src/components/SocialBtn.tsx
@@ -9,8 +9,7 @@ export default function (props: {
   return (
     <div
       style={{ backgroundColor: props.color }}
-      className="flex-none flex items-center justify-center rounded-md text-white p-2 px-3 mr-2 text-center transition-colors duration-1000 ease-in-out"
-    >
+      className="flex-none flex items-center justify-center rounded-md text-white p-2 px-3 mr-2 text-center transition-colors duration-1000 ease-in-out">
       <a id={props.id} href={props.href} target="_blank">
         {props.children}
       </a>
