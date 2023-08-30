c3b4002ae01e4a8b040e9ea3049a910f1e41b0c3
hixcoder <hixcoder@gmail.com>
2023-08-30 10:20:45 +0100

    finish quoteMachine project

--- assets/dynamic-sites/quoteMachine/src/App.tsx
+++ assets/dynamic-sites/quoteMachine/src/App.tsx
@@ -2,48 +2,49 @@ import { useState } from "react";
 import Quote from "./components/Quote";
 import SocialBtn from "./components/SocialBtn";
 import { FaTwitter } from "react-icons/fa";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaTumblr } from "react-icons/fa";
 
 export default function App() {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const quotes = [
+    ["Education costs money. But then so does ignorance.", "Sir Claus Moser"],
+    [
+      "Go confidently in the direction of your dreams. Live the life you have imagined.",
+      "Henry David Thoreau",
+    ],
+    [
+      "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
+      "Dalai Lama",
+    ],
+    [
+      "The only limit to our realization of tomorrow will be our doubts of today.",
+      "Franklin D. Roosevelt",
+    ],
+    ["The secret of getting ahead is getting started.", "Mark Twain"],
+    [
+      "The way to get started is to quit talking and begin doing.",
+      "Walt Disney",
+    ],
+    ["Don't watch the clock; do what it does. Keep going.", "Sam Levenson"],
+    [
+      "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+      "Winston Churchill",
+    ],
+    ["Believe you can and you're halfway there.", "Theodore Roosevelt"],
+    [
+      "Your time is limited, don't waste it living someone else's life.",
+      "Steve Jobs",
+    ],
+    [
+      "The future belongs to those who believe in the beauty of their dreams.",
+      "Eleanor Roosevelt",
+    ],
+  ];
+  const [quote, setQuote] = useState(quotes[0][0]);
+  const [author, setAuthor] = useState(quotes[0][1]);
   const [myColor, setMyColor] = useState("#B93A95");
 
   function getRandomQuote() {
-    const quotes = [
-      ["Education costs money. But then so does ignorance.", "Sir Claus Moser"],
-      [
-        "Go confidently in the direction of your dreams. Live the life you have imagined.",
-        "Henry David Thoreau",
-      ],
-      [
-        "Remember that not getting what you want is sometimes a wonderful stroke of luck.",
-        "Dalai Lama",
-      ],
-      [
-        "The only limit to our realization of tomorrow will be our doubts of today.",
-        "Franklin D. Roosevelt",
-      ],
-      ["The secret of getting ahead is getting started.", "Mark Twain"],
-      [
-        "The way to get started is to quit talking and begin doing.",
-        "Walt Disney",
-      ],
-      ["Don't watch the clock; do what it does. Keep going.", "Sam Levenson"],
-      [
-        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-        "Winston Churchill",
-      ],
-      ["Believe you can and you're halfway there.", "Theodore Roosevelt"],
-      [
-        "Your time is limited, don't waste it living someone else's life.",
-        "Steve Jobs",
-      ],
-      [
-        "The future belongs to those who believe in the beauty of their dreams.",
-        "Eleanor Roosevelt",
-      ],
-    ];
+    
 
     let random = Math.floor(Math.random() * quotes.length);
     setQuote(quotes[random][0]);
@@ -71,11 +72,14 @@ export default function App() {
         <Quote quote={quote} author={author} color={myColor} />
 
         <div className="flex flex-row mt-8">
-          <SocialBtn id="tweet-quote" color={myColor}>
+          <SocialBtn id="tweet-quote" color={myColor} href={`https://twitter.com/intent/tweet?text="${quote}"%0A - ${author}`}>
             <FaTwitter />
           </SocialBtn>
-          <SocialBtn id="tweet-quote" color={myColor}>
-            <FaLinkedinIn />
+          <SocialBtn id="tweet-quote" color={myColor} href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=best_quotes&caption=${author}&content=${quote}.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}>
+
+
+            
+            <FaTumblr />
           </SocialBtn>
 
           <div className="flex-1 rounded-md text-white px-3  text-right">
--- assets/dynamic-sites/quoteMachine/src/components/SocialBtn.tsx
+++ assets/dynamic-sites/quoteMachine/src/components/SocialBtn.tsx
@@ -4,13 +4,14 @@ export default function (props: {
   children: ReactNode;
   id: string;
   color: string;
+  href: string;
 }) {
   return (
     <div
       style={{ backgroundColor: props.color }}
       className="flex-none flex items-center justify-center rounded-md text-white p-2 px-3 mr-2 text-center transition-colors duration-1000 ease-in-out"
     >
-      <a id={props.id} href="#">
+      <a id={props.id} href={props.href} target="_blank">
         {props.children}
       </a>
     </div>
